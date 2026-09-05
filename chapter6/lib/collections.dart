13ba1d8e63f3326c5b1e9b27fbf71c6e488ad7b9
L.W. <6912915+lwinch2006@users.noreply.github.com>
2026-09-05 22:07:15 +0200

    Commiting updated Chapter 6

--- chapter6/lib/collections.dart
+++ chapter6/lib/collections.dart
@@ -1,4 +1,3 @@
-
 import 'dart:collection';
 
 void collections1() {
@@ -22,7 +21,7 @@ void collections1() {
   final list5 = List<int>.empty();
   print('list5: $list5');
 
-  final list6 = [1,2,3];
+  final list6 = [1, 2, 3];
   final list7 = List<int>.unmodifiable(list6);
   final list8 = UnmodifiableListView<int>(list6);
   print('list6 (before change): $list6');
@@ -43,7 +42,7 @@ void collections1() {
   ];
   print('list10: $list10');
 
-  const list11 = [1,2,3];
+  const list11 = [1, 2, 3];
   List<int>? list12;
 
   const list13 = [...list11, 4, 5];
@@ -54,22 +53,27 @@ void collections1() {
   final list15 = [0, ...?list12, ...list11];
   print('list15: $list15');
 
-  final list16 = [1,2,3];
-  list16..add(4)..add(5);
+  final list16 = [1, 2, 3];
+  list16
+    ..add(4)
+    ..add(5);
   print('list16: $list16');
 }
 
 void collections2() {
   print('sets');
-  const set1 = {1,2,3,4,5};
+  const set1 = {1, 2, 3, 4, 5};
   print('set1: $set1');
 
-  final set2 = <int>{}..add(1)..add(2)..add(3);
+  final set2 = <int>{}
+    ..add(1)
+    ..add(2)
+    ..add(3);
 
   final set3 = <int>{};
   final set4 = Set<int>.identity();
 
-  final set5 = {1,2,3};
+  final set5 = {1, 2, 3};
   final set6 = Set<int>.unmodifiable(set5);
   final set7 = UnmodifiableSetView(set5);
   print('set5 (before change): $set5');
@@ -81,7 +85,7 @@ void collections2() {
   print('set6 (after change): $set6');
   print('set7 (after change): $set7');
 
-  final set8 = {1,2,3};
+  final set8 = {1, 2, 3};
   Set<int>? set9;
 
   final set10 = {0, ...?set9, ...set8};
@@ -89,10 +93,7 @@ void collections2() {
   print('set10[2]: ${set10.elementAt(2)}');
 
   const condition1 = true;
-  final set11 = {
-    for (var i = 1; i < 5; i++) i,
-    if (condition1) 10
-  };
+  final set11 = {for (var i = 1; i < 5; i++) i, if (condition1) 10};
   print('set11: $set11');
 }
 
@@ -106,16 +107,13 @@ void collections3() {
   final map3 = <String, int>{};
   final map4 = {};
 
-  final list1 = [1,2,3];
-  final map5 = Map<String, int>.fromIterable(
-    list1,
-    key: (element) => element.toString(),
-    value: (element) => element
-  );
+  final list1 = [1, 2, 3];
+  final map5 = Map<String, int>.fromIterable(list1,
+      key: (element) => element.toString(), value: (element) => element);
   print('map5: $map5');
 
   final list2 = ['item1', 'item2', 'item3'];
-  final list3 = [1,2,3];
+  final list3 = [1, 2, 3];
 
   final map6 = Map<String, int>.fromIterables(list2, list3);
   print('map6: $map6');
@@ -136,14 +134,14 @@ void collections3() {
   print('map9: $map9');
 
   final map10 = {
-    for (var i = 1; i < 5; i++) 'item $i' : i,
+    for (var i = 1; i < 5; i++) 'item $i': i,
   };
   print('map10: $map10');
 
   final map11 = {'1': 1, '2': 2, '3': 3};
   map11
-  ..update('1', (value) => 11)
-  ..update('4', (value) => 44, ifAbsent: () => 44);
+    ..update('1', (value) => 11)
+    ..update('4', (value) => 44, ifAbsent: () => 44);
 
   print('map11: $map11');
 }
@@ -151,8 +149,8 @@ void collections3() {
 void collections4() {
   print('of() and from()');
 
-  final list1 = List<int>.of([1,2,3]);
-  final list2 = <Object>[1,2,3];
+  final list1 = List<int>.of([1, 2, 3]);
+  final list2 = <Object>[1, 2, 3];
   final list3 = List<int>.from(list2);
 
   print('list1: $list1');
@@ -171,7 +169,8 @@ void collections5() {
       .toList();
 
   final value1 = list1.reduce((value, element) => value + element);
-  final value2 = list1.fold<int>(0, (currentNumber, element) => currentNumber + '$element'.length);
+  final value2 = list1.fold<int>(
+      0, (currentNumber, element) => currentNumber + '$element'.length);
 
   print('list1: $list1');
   print('list2: $list2');
@@ -179,13 +178,9 @@ void collections5() {
   print('value2: $value2');
 }
 
-class A {
+class A {}
 
-}
-
-class B extends A {
-
-}
+class B extends A {}
 
 void collections6() {
   final List<A> list1 = <A>[];
@@ -193,16 +188,22 @@ void collections6() {
   // final List<B> list3 = <A>[]; // Compile time error.
 }
 
+void collections7() {
+  // Fixed length unmodifiable list as const variable
+  const testList1 = [1, 2, 3];
+  print('testList1: $testList1');
 
+  // Fixed length unmodifiable list as final variable
+  final testList3 = List<int>.unmodifiable([1, 2, 3]);
+  print('testList3: $testList3');
 
+  // Fixed length modifiable list as final variable
+  final testList2 = List<int>.of([1, 2, 3], growable: false);
+  testList2[0] = 111;
+  print('testList2: $testList2');
 
-
-
-
-
-
-
-
-
-
-
+  // Growable modifiable list as final variable
+  final testList4 = [1, 2, 3];
+  testList4.add(4);
+  print('testList4: $testList4');
+}
